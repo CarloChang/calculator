6652e137ce93b7ce652bf79a1daf886b0be2ec03
Carlo Chang <carlochang123@hotmail.com>
2023-08-16 22:00:54 +0200

    Added function to some buttons. Added a display whenever a button is used

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 function adition (a,b) {
+    displayElement.textContent = parseInt(a) + parseInt(b);
+    displayValue = ""; 
     return a + b;
 }
 
 function subtraction(a,b) {
+    displayElement.textContent = a - b;
+    displayValue = ""; 
     return a - b;
 }
 
 function multiplication(a,b) {
+    displayElement.textContent = a * b;
+    displayValue = ""; 
     return a * b;
+    
 }
 
 function division (a,b) {
-    const result = a / b
+    const result = a / b;
+    displayElement.textContent = parseFloat(result.toFixed(2));
+    displayValue = ""; 
     return parseFloat(result.toFixed(2));
 }
 
@@ -19,26 +28,41 @@ function operate(firstValue, operator, secondValue) {
    return operator(firstValue, secondValue)
 }
 
-function test(e){
-    one.addEventListener('click')
+let displayValue = "";
+
+function deleteEverything() {
+    displayElement.textContent = "";
+    displayValue = ""; 
 }
 
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
-const zero = document.getElementById('zero');
+function updateDisplay(value) {
+    displayElement.textContent = value; 
+}
+
+const numberButton = document.querySelectorAll('.number');
+numberButton.forEach(button => {
+    button.addEventListener('click', () => {
+        displayValue += button.id;
+        updateDisplay(displayValue);
+    });
+});
+
+
+
+const displayElement = document.getElementById('output');
 const plus = document.getElementById('plus');
 const minus = document.getElementById('minus');
 const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
-const equals = document.getElementById('one');
+const equals = document.getElementById('equals');
 const deleteButton = document.getElementById('delete');
+deleteButton.addEventListener("click", () => deleteEverything());
+equals.addEventListener('click', () => operate(displayValue,subtraction,3));
 
-console.log(operate(1,division,4))
-console.log(division(10,3))
+const buttons = document.querySelectorAll('button');
+buttons.addEventListener('animationend');
+
+
+
+
+
